c33d41ab568ba817a8e693e1e4763040033b843d
Red <crabmarket56@gmail.com>
2022-07-17 08:48:14 -0400

    Add player guessing method

--- script.rb
+++ script.rb
@@ -2,22 +2,31 @@
 # human receives feedback upon each guess
 require 'pry-byebug'
 
-class Computer
+class Player
   @@peg_options = ['red', 'blue', 'purple', 'green', 'pink', 'blank']
   @@selected_pegs = []
+  @@guessed_pegs = []
+end
 
+class Computer < Player
   def select_pegs
     for i in 0..3
     color = @@peg_options.sample
     @@selected_pegs.push(color)
-    binding.pry
     end
   end
 end
 
-class Human
-
+class Human < Player
+  def guess_pegs
+    for i in 0..3
+      p "Given #{@@peg_options}, please input your ##{i} guess"
+      @@guessed_pegs[i] = gets.chomp.downcase
+    end
+  end
 end
 
 computer_player = Computer.new
+human_player = Human.new
 computer_player.select_pegs
+human_player.guess_pegs
