102865f314c015de3770ddd24a266f3ba5a47d2e
Red <crabmarket56@gmail.com>
2022-07-17 14:12:48 -0400

    Allow player to make the code

--- script.rb
+++ script.rb
@@ -8,10 +8,12 @@ class Player
 end
 
 class Computer < Player
-  def select_pegs
-    for i in 0..3
-      color = @@peg_options.sample
-      @@selected_pegs.push(color)
+  def select_pegs?
+    if @@selected_pegs.length == 0
+      for i in 0..3
+        color = @@peg_options.sample
+        @@selected_pegs.push(color)
+      end
     end
   end
 end
@@ -42,11 +44,30 @@ class Human < Player
       'Congratulations, you have guessed the peg order correctly and beat the computer!'
     end
   end
+
+  def select_role
+    p "If you would like to be the code-maker, type 'y' and select enter, otherwise, enter anything else to be the code-breaker"
+    user_selection = gets.chomp.downcase
+    select_pegs if user_selection == 'y'
+  end
+
+  def select_pegs
+    for i in 0..3
+      p "Input the color you would like to place at position #{i} and press enter. If your input is not an option, you will have to restart."
+      color = gets.chomp.downcase
+      if @@peg_options.include?(color)
+        @@selected_pegs.push(color)
+      else
+        select_pegs
+      end
+    end
+  end
 end
 
 computer_player = Computer.new
 human_player = Human.new
-computer_player.select_pegs
+human_player.select_role
+computer_player.select_pegs?
 
 i = 0
 while i < 12
