f60ae8fcb9292dd11636b62c21216bd5d3abf0aa
Red <crabmarket56@gmail.com>
2022-07-17 11:50:12 -0400

    Add CLI user guess feedback

--- script.rb
+++ script.rb
@@ -1,5 +1,5 @@
 # computer selects 4 colors out of 6 options and places them in selection order. human has 12 guesses to guess the secret code. 
-# human receives feedback upon each guess
+# human receives feedback detailing whether the color was correct, the color and order were correct, or neither upon each guess.
 require 'pry-byebug'
 
 class Player
@@ -22,8 +22,16 @@ class Human < Player
     for i in 0..3
       p "Given #{@@peg_options}, please input your ##{i} guess"
       @@guessed_pegs[i] = gets.chomp.downcase
+
+      if @@selected_pegs[i] == @@guessed_pegs[i]
+        p "Your guess of #{@@guessed_pegs[i]} at position #{i} is correct!"
+      elsif  @@selected_pegs.include?(@@guessed_pegs[i])
+        p "Your guess of #{@@guessed_pegs[i]} is included in the computer's selection, but the position #{i} is incorrect."
+      else
+        p "your guess of #{@@guessed_pegs[i]} is not included in the computer's selection."        
     end
   end
+end
 
   def winner?
     if @@guessed_pegs == @@selected_pegs
