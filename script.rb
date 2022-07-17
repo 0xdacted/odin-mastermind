e37e7fbc3aada030b011f0bb1c4fa1eb9a24a8b5
Red <crabmarket56@gmail.com>
2022-07-17 13:40:51 -0400

    Fix styling and remove printing

--- script.rb
+++ script.rb
@@ -1,8 +1,8 @@
-# computer selects 4 colors out of 6 options and places them in selection order. human has 12 guesses to guess the secret code. 
+# computer selects 4 colors out of 6 options and places them in selection order. human has 12 guesses to guess the secret code.
 # human receives feedback detailing whether the color was correct, the color and order were correct, or neither upon each guess.
 require 'pry-byebug'
 class Player
-  @@peg_options = ['red', 'blue', 'purple', 'green', 'pink', 'blank']
+  @@peg_options = %w[red blue purple green pink blank]
   @@selected_pegs = []
   @@guessed_pegs = []
 end
@@ -10,8 +10,8 @@ end
 class Computer < Player
   def select_pegs
     for i in 0..3
-    color = @@peg_options.sample
-    @@selected_pegs.push(color)
+      color = @@peg_options.sample
+      @@selected_pegs.push(color)
     end
   end
 end
@@ -20,26 +20,26 @@ class Human < Player
   def guess_pegs
     for i in 0..3
       p "Given #{@@peg_options}, please input your ##{i} guess"
-      p user_guess = gets.chomp.downcase 
+      user_guess = gets.chomp.downcase
       if @@peg_options.include?(user_guess)
-         @@guessed_pegs[i] = user_guess
-      else 
-        self.guess_pegs
+        @@guessed_pegs[i] = user_guess
+      else
+        guess_pegs
       end
 
       if @@selected_pegs[i] == @@guessed_pegs[i]
         p "Your guess of #{@@guessed_pegs[i]} at position #{i} is correct!"
-      elsif  @@selected_pegs.include?(@@guessed_pegs[i])
+      elsif @@selected_pegs.include?(@@guessed_pegs[i])
         p "Your guess of #{@@guessed_pegs[i]} is included in the computer's selection, but the position #{i} is incorrect."
       else
-        p "your guess of #{@@guessed_pegs[i]} is not included in the computer's selection."        
-    end
+        p "your guess of #{@@guessed_pegs[i]} is not included in the computer's selection."
+      end
   end
-end
+  end
 
   def winner?
     if @@guessed_pegs == @@selected_pegs
-      "Congratulations, you have guessed the peg order correctly and beat the computer!"
+      'Congratulations, you have guessed the peg order correctly and beat the computer!'
     end
   end
 end
@@ -51,10 +51,10 @@ computer_player.select_pegs
 i = 0
 while i < 12
   human_player.guess_pegs
-  i+= 1
+  i += 1
   if human_player.winner?
     p human_player.winner?
-  break
+    break
   else
     p "Your guessed order is incorrect! you have #{12 - i} tries remaining"
   end
