39f4253f89413f24e1b5a029b6453c338f69affe
Red <crabmarket56@gmail.com>
2022-07-17 08:34:29 -0400

    Fix peg selection to select 4, initialize computer_player

--- script.rb
+++ script.rb
@@ -1,17 +1,23 @@
 # computer selects 4 colors out of 6 options and places them in selection order. human has 12 guesses to guess the secret code. 
 # human receives feedback upon each guess
+require 'pry-byebug'
 
 class Computer
-  peg_options = [red, blue, purple, green, pink, blank]
+  @@peg_options = ['red', 'blue', 'purple', 'green', 'pink', 'blank']
   @@selected_pegs = []
 
   def select_pegs
-    for i in 0..4
-    color = peg_options.sample
-    selected_pegs.push(color)
+    for i in 0..3
+    color = @@peg_options.sample
+    @@selected_pegs.push(color)
+    binding.pry
+    end
   end
 end
 
 class Human
 
 end
+
+computer_player = Computer.new
+computer_player.select_pegs
